08ea24da3ead006193a9a8835765d9b4c38e646d
Gazi <nafiul00718@gmail.com>
2026-03-12 17:00:42 -0400

    Added a function

--- hardproblem.py
+++ hardproblem.py
@@ -1,1 +1,10 @@
 #this is a hard problem
+class HardProblem(str):
+    def __init__(self, value):
+        super().__init__()
+        self.value = value
+
+    def solve(self):
+        # Implement a complex algorithm to solve the problem
+        result = self.value ** 2  # Placeholder for a more complex operation
+        return result
