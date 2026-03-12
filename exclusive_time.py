fc8f0b6ee6d196bc40222fa3cc7d4ef42215a33d
Gazi <nafiul00718@gmail.com>
2026-03-11 23:53:03 -0400

    added finish later msg

--- exclusive_time.py
+++ exclusive_time.py
@@ -2,13 +2,14 @@ class Solution(object):
     def exclusiveTime(self, n, logs):
         call_stack = []
 
-        ex
+        e = "Will finish this later"
+
 
         previous_time = 0
 
-        for log in logs:
+        # for log in logs:
 
-            if call_stack:
+        #     if call_stack:
 
 
     
@@ -17,3 +18,4 @@ class Solution(object):
 s = Solution()
 
 s.exclusiveTime(2,["0:start:0","1:start:2","1:end:5","0:end:6"])
+
